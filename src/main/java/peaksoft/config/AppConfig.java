926bfd3880801cf8a723eb5f324dd8a6b323588e
erlan <eomabekov@gmail.com>
2022-02-14 02:14:52 +0300

    I have completed this assignment!

--- src/main/java/peaksoft/config/AppConfig.java
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -1,9 +1,22 @@
 package peaksoft.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import peaksoft.model.Animal;
+import peaksoft.model.Timer;
 
 @Configuration
 @ComponentScan(basePackages = "peaksoft")
 public class AppConfig {
+    @Bean
+    public Animal animal(){
+        return new Animal();
+    }
+
+
+    @Bean
+    public Timer timer(){
+        return new Timer();
+    }
 }
--- src/main/java/peaksoft/model/AnimalsCage.java
+++ src/main/java/peaksoft/model/AnimalsCage.java
@@ -1,13 +1,34 @@
 package peaksoft.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("singleton")
 public class AnimalsCage {
 
+    private Timer timer;
+
     @Autowired
+    @Qualifier("dog")
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
     private Animal animal;
+
+    public Timer getTimer(){
+        return timer;
+    }
+
+    @Autowired
+    @Qualifier("timer")
+public void setTimer(Timer timer){
+        this.timer = timer;
+}
+
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
