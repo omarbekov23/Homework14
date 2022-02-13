cbd2e9a2d866f7090b3e2f57a713f7a0456f33b5
erlan <eomabekov@gmail.com>
2022-02-14 02:14:52 +0300

    I have completed this assignment!

--- src/main/java/peaksoft/model/Timer.java
+++ src/main/java/peaksoft/model/Timer.java
@@ -1,10 +1,18 @@
 package peaksoft.model;
 
+
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Timer{" +
+                "nanoTime=" + nanoTime +
+                '}';
     }
 }
